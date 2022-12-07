3e0878f489b9d493a841cfc5274408c39c2fb1f6
Thomas McKeown <tmckeown98@gmail.com>
2022-12-07 19:06:01 +0000

    Day 1 completed

--- src/days/Day1.tsx
+++
@@ -1,5 +0,0 @@
-export default function Day1() {
-  return (
-    <></>
-  )
-}
--- src/page/DayPage.tsx
+++ src/page/DayPage.tsx
@@ -1,21 +1,29 @@
 import { useParams } from "react-router-dom";
 import {
+  Alert,
   Box
 } from "@mui/material";
-import Day1 from "../days/Day1";
+import Day1 from "../days/Day1/Day1";
 
 function getDayComponent(index: Number) {
   switch (index) {
     case 1: return <Day1 />
+    default:
+      return <Alert severity="error">Day index must be a number</Alert>
   }
 }
 
 export default function DayPage() {
   const { dayId } = useParams();
 
+  if (dayId === undefined) {
+    return <Alert severity="error">Day index is undefined</Alert>
+  }
+  const index: Number = parseInt(dayId);
+  
   return (
     <Box sx={{ mx: 20, my: 5 }}>
-      DAY { dayId }
+      {getDayComponent(index)}
     </Box>
   )
 }
