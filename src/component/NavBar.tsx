ea54f016a493ce5bf9e3ce6753a7870fd71efb28
Thomas McKeown <tmckeown98@gmail.com>
2022-12-10 13:48:48 +0000

    Clean up

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -7,7 +7,12 @@ import {
 export default function NavBar() {
   return (
     <Box mx={2}>
-      <Link href="#"><Typography variant="h4" gutterBottom>Home</Typography></Link>
+      <Link href="#">
+        <Typography variant="h4" gutterBottom>Home</Typography>
+      </Link>
+      <Link href="https://github.com/tmckeown2/advent-of-code-2022">
+        <Typography variant="h4" gutterBottom>Project</Typography>
+      </Link>
     </Box>
   );
 }
